db8bc2e620ffabb1755e34c13f0a41de66ec35f3
GT <108051083+GreeshmaThadana@users.noreply.github.com>
2024-01-06 19:26:45 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+
+if __name__ == '__main__':
+    print("Welcome to robo speaker 1.1 created by Greesh")
+    while(True):
+        x = input("enter what you want to say: ")
+        if x == "q":
+            os.system("say 'bye bye friend'")
+            break
+        command = f"Say {x}"
+        os.system(command)
+print("End of talking")
